70cdae9db36a5bedab446e192f36aefcde9e730a
gcaule <germain.caule@gmail.com>
2018-01-07 20:26:33 +0100

    Added Death, changed chapters navigation system, fixed bugs and kinks

--- app/src/main/java/com/kgames/james/nerdybook/PlayLuck.java
+++ app/src/main/java/com/kgames/james/nerdybook/PlayLuck.java
@@ -1,6 +1,8 @@
 package com.kgames.james.nerdybook;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +11,7 @@ import android.widget.TextView;
 
 import com.kgames.james.nerdybook.Hero.HeroDBHelper;
 
-public class PlayLuckActivity extends AppCompatActivity {
+public class PlayLuck extends AppCompatActivity {
 
     HeroDBHelper mHeroDBHelper;
 
@@ -18,6 +20,8 @@ public class PlayLuckActivity extends AppCompatActivity {
     int mTotalChapters;
     int mLuckCurrent;
 
+    int mAbilityLoss;
+
     boolean mLuckOfFate;
 
     @Override
@@ -25,7 +29,7 @@ public class PlayLuckActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_luck);
 
-        mHeroDBHelper = new HeroDBHelper(PlayLuckActivity.this);
+        mHeroDBHelper = new HeroDBHelper(PlayLuck.this);
 
         mHeroID = getIntent().getExtras().getString("HeroID");
         mCurrentChapter = getIntent().getExtras().getInt("CurrentChapter");
@@ -41,6 +45,76 @@ public class PlayLuckActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 mLuckOfFate = mHeroDBHelper.isHeroLucky(mHeroID);
+
+                if (mCurrentChapter == 159) {
+
+                    if (mLuckOfFate) {
+
+                        playYourLuck.setVisibility(View.GONE);
+                        playedLuckButton.setVisibility(View.VISIBLE);
+
+                        mLuckCurrent = mHeroDBHelper.currentLuck(mHeroID);
+                        mCurrentChapter = 267;
+
+                        if (mLuckCurrent == 0) {
+                            playedLuckText.setText(getString(R.string.hero_luck_played_null));
+                        } else {
+                            playedLuckText.setText(String.format(getString(R.string.hero_luck_played),
+                                    String.valueOf(mLuckCurrent)));
+                        }
+
+                        playedLuckButton.setText(getString(R.string.talisman_chapter203_choice1));
+
+                        playedLuckButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                Intent intent = new Intent(PlayLuck.this, AdventureTalisman.class);
+                                intent.putExtra("HeroID", mHeroID);
+                                intent.putExtra("CurrentChapter", mCurrentChapter);
+                                intent.putExtra("TotalChapters", mTotalChapters + 1);
+                                startActivity(intent);
+
+                            }
+                        });
+
+
+                    } else {
+
+                        playYourLuck.setVisibility(View.GONE);
+                        playedLuckButton.setVisibility(View.VISIBLE);
+
+                        mLuckCurrent = mHeroDBHelper.currentLuck(mHeroID);
+                        mCurrentChapter = 135;
+
+                        if (mLuckCurrent == 0) {
+                            playedLuckText.setText(getString(R.string.hero_luck_played_null));
+                        } else {
+                            playedLuckText.setText(String.format(getString(R.string.hero_luck_played),
+                                    String.valueOf(mLuckCurrent)));
+                        }
+
+                        playedLuckButton.setText(getString(R.string.talisman_chapter203_choice2));
+
+                        playedLuckButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                mAbilityLoss = 1;
+                                mHeroDBHelper.currentAbilityLoss(mHeroID, mAbilityLoss);
+
+                                Intent intent = new Intent(PlayLuck.this, AdventureTalisman.class);
+                                intent.putExtra("HeroID", mHeroID);
+                                intent.putExtra("CurrentChapter", mCurrentChapter);
+                                intent.putExtra("TotalChapters", mTotalChapters + 1);
+                                startActivity(intent);
+
+                            }
+                        });
+
+                    }
+
+                }
 
                 if (mCurrentChapter == 203) {
 
@@ -65,7 +139,7 @@ public class PlayLuckActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View v) {
 
-                                Intent intent = new Intent(PlayLuckActivity.this, AdventureTalisman.class);
+                                Intent intent = new Intent(PlayLuck.this, AdventureTalisman.class);
                                 intent.putExtra("HeroID", mHeroID);
                                 intent.putExtra("CurrentChapter", mCurrentChapter);
                                 intent.putExtra("TotalChapters", mTotalChapters + 1);
@@ -96,7 +170,7 @@ public class PlayLuckActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View v) {
 
-                                Intent intent = new Intent(PlayLuckActivity.this, AdventureTalisman.class);
+                                Intent intent = new Intent(PlayLuck.this, AdventureTalisman.class);
                                 intent.putExtra("HeroID", mHeroID);
                                 intent.putExtra("CurrentChapter", mCurrentChapter);
                                 intent.putExtra("TotalChapters", mTotalChapters + 1);
@@ -113,4 +187,17 @@ public class PlayLuckActivity extends AppCompatActivity {
         });
 
     }
+
+    // Back to adventure
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(PlayLuck.this, AdventureTalisman.class);
+        intent.putExtra("HeroID", String.valueOf(mHeroID));
+        intent.putExtra("CurrentChapter", mCurrentChapter);
+        intent.putExtra("TotalChapters", mTotalChapters);
+        startActivity(intent);
+
+    }
+
 }
